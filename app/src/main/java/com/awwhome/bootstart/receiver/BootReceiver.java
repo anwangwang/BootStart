f918c0052e72979b773563560f16f38ce6f1aae1
anwangwang <wangwang@tc2025.com>
2017-03-03 17:54:31 +0800

    Initial commit

---
+++ app/src/main/java/com/awwhome/bootstart/receiver/BootReceiver.java
@@ -0,0 +1,20 @@
+package com.awwhome.bootstart.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2017/3/3 0003.
+ */
+
+public class BootReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "BootReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive: onReceive方法被执行了 ");
+    }
+}
